005394959d3ec093477c97a6ebc5bb2fec2b72db
Keira Nevers <keira.nevers@gmail.com>
2018-03-19 18:02:09 -0400

    commit express.static

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static('public'));
 
 //ROUTER
 require('./app/routing/apiRoutes.js')(app); 
